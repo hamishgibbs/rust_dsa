5bcbbcc93fa5ed1411ab8c894520fc2ffa99b6f1
hamishgibbs <hamishgibbs@gmail.com>
2021-10-14 07:34:52 +0100

    edit distance:

---
+++ src/bin/edit_distance.rs
@@ -0,0 +1,65 @@
+/*
+
+How it works:
+
+Calculates the edit distance (a string distance metric) for two strings.
+
+Edit distance is the number of changes needed to make two strings equal.
+
+i.e.
+
+(test, tset) has an edit distance of one.
+
+*/
+
+use std::cmp::min;
+
+pub fn edit_distance(str_a: &str, str_b: &str) -> u32 {
+    // Initialize a vector of distances [i][j] for a[..i] & b[..i]
+    let mut distances = vec![vec![0; str_b.len() + 1]; str_a.len() + 1];
+
+    // Initialise top row and left column of matrix with ascending integers
+    for j in 0..=str_b.len() {
+        distances[0][j] = j as u32;
+    }
+    for (i, item) in distances.iter_mut().enumerate() {
+        item[0] = i as u32;
+    }
+    for i in 1..=str_a.len() {
+        for j in 1..=str_b.len() {
+            distances[i][j] = min(distances[i - 1][j] + 1, distances[i][j - 1] + 1);
+            if str_a.as_bytes()[i - 1] == str_b.as_bytes()[j - 1] {
+                distances[i][j] = min(distances[i][j], distances[i - 1][j - 1]);
+            } else {
+                distances[i][j] = min(distances[i][j], distances[i - 1][j - 1] + 1);
+            }
+        }
+    }
+    distances[str_a.len()][str_b.len()]
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn equal_strings() {
+        assert_eq!(0, edit_distance("Hello, world!", "Hello, world!"));
+        assert_eq!(0, edit_distance("Test_Case_#1", "Test_Case_#1"));
+    }
+
+    #[test]
+    fn one_edit_difference() {
+        assert_eq!(1, edit_distance("Hello, world!", "Hell, world!"));
+        assert_eq!(1, edit_distance("Test_Case_#1", "Test_Case_#2"));
+        assert_eq!(1, edit_distance("Test_Case_#1", "Test_Case_#10"));
+    }
+
+    #[test]
+    fn several_differences() {
+        assert_eq!(2, edit_distance("My Cat", "My Case"));
+        assert_eq!(7, edit_distance("Hello, world!", "Goodbye, world!"));
+        assert_eq!(6, edit_distance("Test_Case_#3", "Case #3"));
+    }
+}
